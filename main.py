12494aa912037f4785b8664e1c7b6f61e88f82d2
Keir Little <me@keirlittle.com>
2015-02-02 11:17:13 +0000

    basic post adding and showing functionality added

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, request, render_template, flash, redirect, url_for
+from google.appengine.ext import ndb
 app = Flask(__name__)
+app.debug = True
+
+SECRET_KEY = "lol wut"
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+class Post(ndb.Model):
+    ident = ndb.IntegerProperty()
+    title = ndb.StringProperty(required=True)
+    content = ndb.StringProperty(required=True)
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return redirect(url_for("show"))
 
+@app.route('/show')
+def show():
+    posts = Post.query()
+    return render_template("show.html", posts=posts)
+
+@app.route("/add")
+def post_form():
+    return render_template("post.html")
+
+@app.route("/post", methods=["POST"])
+def add_post():
+    post = Post(title=request.form["title"], content=request.form["content"])
+    post_key = post.put()
+    
+    return render_template("posted.html", post_key=post_key)
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+    return 'Sorry, nothing at this URL.', 404
 
 
 @app.errorhandler(500)
